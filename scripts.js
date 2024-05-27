e957794af44932b09d4f4dda7d44d98378627dbc
Eduardo Rubbini <91645742+rubbini-en@users.noreply.github.com>
2024-05-27 19:34:28 -0400

    Update

--- scripts.js
+++ scripts.js
@@ -1,6 +1,23 @@
 document.addEventListener('DOMContentLoaded', (event) => {
     const toggleButton = document.getElementById('darkModeToggle');
+    const darkModeIcon = document.getElementById('darkModeIcon');
+    const backToTopButton = document.getElementById('backToTop');
+
     toggleButton.addEventListener('click', () => {
         document.body.classList.toggle('dark-mode');
+        darkModeIcon.classList.toggle('fa-moon');
+        darkModeIcon.classList.toggle('fa-sun');
+    });
+
+    window.addEventListener('scroll', () => {
+        if (window.scrollY > 200) {
+            backToTopButton.classList.add('show');
+        } else {
+            backToTopButton.classList.remove('show');
+        }
+    });
+
+    backToTopButton.addEventListener('click', () => {
+        window.scrollTo({ top: 0, behavior: 'smooth' });
     });
 });
